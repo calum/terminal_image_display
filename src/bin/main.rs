7695754ae2badd14f01b4a403249b14fd9e35731
calum <calumforster@live.co.uk>
2018-02-12 18:07:39 +0000

    adding support for gifs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,7 +11,7 @@ use terminal_graphics::Colour;
 
 use terminal_size::{Width, Height, terminal_size};
 
-use termage::{get_image, pixelate_image};
+use termage::{display_image, display_gif};
 
 use clap::{Arg, App};
 
@@ -25,75 +25,29 @@ fn main() {
                             .long("image")
                             .value_name("FILE")
                             .help("Input image filepath")
-                            .required(true)
+                            .conflicts_with("gif")
+                            .takes_value(true))
+                    .arg(Arg::with_name("gif")
+                            .short("g")
+                            .long("gif")
+                            .value_name("FILE")
+                            .help("Input animated gif filepath")
                             .takes_value(true))
                     .get_matches();
 
-    let image_filepath = matches.value_of("image").unwrap();
+    let image_filepath = matches.value_of("image");
+    let gif_filepath = matches.value_of("gif");
 
     let size = terminal_size();
+
     if let Some((Width(w), Height(h))) = size {
-        display_image(image_filepath, w as u32, h as u32);
+        if let Some(filepath) = image_filepath {
+            display_image(filepath, w as u32, h as u32);
+        } else if let Some(filepath) = gif_filepath {
+            display_gif(filepath, w as u32, h as u32);
+        }
     } else {
         println!("Error: Unable to get terminal dimensions.");
     }
-}
 
-fn display_image(image_filepath: &str, width: u32, height: u32) {
-    let mut img = get_image(image_filepath);
-
-    // get the image dimensions
-    let (img_width, img_height) = img.dimensions();
-    let ratio = (img_width as f32)/(img_height as f32);
-
-    let mut display_height = height;
-    let mut display_width = width;
-
-    // scale the image to the correct dimensions
-    if ratio > 1.00 {
-        display_height = ((display_width as f32)/(ratio * 2.0)).floor() as u32;
-    } else {
-        display_width = 2 * ((display_height as f32) * ratio).floor() as u32;
-    }
-
-    // scale the image up if it is too small to fit the dimensions of the terminal
-    let mut scale = 1.00;
-    if (display_width as f32)*scale > (img_width as f32) {
-        scale = (img_width as f32)/(width as f32);
-    }
-    if (display_height as f32)*scale > (img_height as f32) {
-        scale = (img_height as f32)/(height as f32);
-    }
-
-    // scale the width and height
-    display_width = ((display_width as f32)*scale).floor() as u32;
-    display_height = ((display_height as f32)*scale).floor() as u32;
-
-    // scale the display to fit the terminal:
-    scale = 1.0;
-    if (display_width as f32) * scale > (width as f32) {
-        scale = (width as f32) / (display_width as f32);
-    }
-    if (display_height as f32) * scale > (height as f32) {
-        scale = (height as f32) / (display_height as f32);
-    }
-
-    // scale the width and height
-    display_width = ((display_width as f32)*scale).floor() as u32;
-    display_height = ((display_height as f32)*scale).floor() as u32;
-
-    let mut screen = Display::new(display_width, display_height);
-    screen.clear();
-
-    let img_out = pixelate_image(&mut img, display_width, display_height);
-
-    for (x, y, pixel) in img_out.enumerate_pixels() {
-        let rgb = pixel.to_rgb().data;
-        let colour = Colour::from_rgb(rgb[0], rgb[1], rgb[2]);
-
-        screen.set_pixel(x as isize, y as isize, ' ', Colour::White, colour);
-    }
-
-    screen.print();
-    println!("");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 extern crate image;
+extern crate terminal_graphics;
+
+use terminal_graphics::Display;
+use terminal_graphics::Colour;
 
 use std::path::Path;
+use std::fs::File;
+use std::{thread, time};
 
-use image::{GenericImage, Pixel, RgbImage};
+use image::{GenericImage, Pixel, RgbImage, gif, Frames, ImageDecoder, Frame};
+use image::{ConvertBuffer, ImageBuffer};
 
 pub fn pixelate_image(img: &mut RgbImage, width: u32, height: u32) -> RgbImage {
     // Create a new ImgBuf
@@ -11,6 +18,16 @@ pub fn pixelate_image(img: &mut RgbImage, width: u32, height: u32) -> RgbImage {
     closest_match(&mut imgbuf, img);
 
     imgbuf
+}
+
+/// Opens the file and reads a gif animated image.
+/// Returns the Frames for the gif.
+pub fn get_gif(filename: &str) -> Frames {
+    let mut f = File::open(filename).expect("File not found");
+
+    let mut decoder = gif::Decoder::new(f);
+
+    decoder.into_frames().expect("error decoding gif")
 }
 
 pub fn get_image(filename: &str) -> RgbImage {
@@ -79,5 +96,86 @@ pub fn merge_pixels(imgbuf: &mut RgbImage, img: &mut RgbImage) {
 
         // Create an 8bit pixel of type RGB
         *pixel = image::Rgb([big_pixel_rgb[0] as u8, big_pixel_rgb[1] as u8, big_pixel_rgb[2] as u8]);
+    }
+}
+
+fn render_image(mut image: RgbImage, width: u32, height: u32) {
+    // get the image dimensions
+    let (img_width, img_height) = image.dimensions();
+    let ratio = (img_width as f32)/(img_height as f32);
+
+    let mut display_height = height;
+    let mut display_width = width;
+
+    // scale the image to the correct dimensions
+    if ratio > 1.00 {
+        display_height = ((display_width as f32)/(ratio * 2.0)).floor() as u32;
+    } else {
+        display_width = 2 * ((display_height as f32) * ratio).floor() as u32;
+    }
+
+    // scale the image up if it is too small to fit the dimensions of the terminal
+    let mut scale = 1.00;
+    if (display_width as f32)*scale > (img_width as f32) {
+        scale = (img_width as f32)/(width as f32);
+    }
+    if (display_height as f32)*scale > (img_height as f32) {
+        scale = (img_height as f32)/(height as f32);
+    }
+
+    // scale the width and height
+    display_width = ((display_width as f32)*scale).floor() as u32;
+    display_height = ((display_height as f32)*scale).floor() as u32;
+
+    // scale the display to fit the terminal:
+    scale = 1.0;
+    if (display_width as f32) * scale > (width as f32) {
+        scale = (width as f32) / (display_width as f32);
+    }
+    if (display_height as f32) * scale > (height as f32) {
+        scale = (height as f32) / (display_height as f32);
+    }
+
+    // scale the width and height
+    display_width = ((display_width as f32)*scale).floor() as u32;
+    display_height = ((display_height as f32)*scale).floor() as u32;
+
+    let mut screen = Display::new(display_width, display_height);
+    screen.clear();
+
+    let img_out = pixelate_image(&mut image, display_width, display_height);
+
+    for (x, y, pixel) in img_out.enumerate_pixels() {
+        let rgb = pixel.to_rgb().data;
+        let colour = Colour::from_rgb(rgb[0], rgb[1], rgb[2]);
+
+        screen.set_pixel(x as isize, y as isize, ' ', Colour::White, colour);
+    }
+
+    screen.print();
+    println!("");
+}
+
+pub fn display_image(image_filepath: &str, width: u32, height: u32) {
+    let mut img = get_image(image_filepath);
+
+    render_image(img, width, height);
+}
+
+pub fn display_gif(gif_filepath: &str, width: u32, height: u32) {
+    // get the original gif
+    let mut frames = get_gif(gif_filepath);
+
+    // create the new reduced gif by shrinking each frame to fit
+    // the terminal
+    for (i, frame) in frames.enumerate() {
+        let mut image = frame.into_buffer();
+
+        // display the image:
+        //render_image(image.convert(), width, height);
+
+        println!("Frame: {}", i);
+        thread::sleep(time::Duration::from_millis(100));
+        println!("next frame...");
     }
 }
